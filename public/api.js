d42605d2997e3cd6b1a174e39cabe0cd27aff61a
Ainetos <airupim@gmail.com>
2025-11-27 10:50:50 +0100

    intento de introducir la api

---
+++ public/api.js
@@ -0,0 +1,9 @@
+const apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,is_day,weather_code';
+
+// Fetch the weather data
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('weather').innerText = `The current temperature in Lleida is ${data.current.temp_c}°C`;
+  })
+  .catch(error => console.error('Error:', error));
