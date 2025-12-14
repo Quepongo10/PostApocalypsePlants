e0115a918cc86f5ef8b38256edbca3ca04c1faa9
Ainetos <airupim@gmail.com>
2025-12-14 13:06:21 +0100

    Climas que faltaban añadidos

--- public/api.js
+++ public/api.js
@@ -2,7 +2,7 @@ export default async function getClima(opts = {}) {
   const cameras = {
     cam1: { lat: 41.607661, lon: 0.62418 }, // Lleida/España
     cam2: { lat: 12.368187, lon: -1.527094 }, // Ouagadougou/Burkina Faso
-    cam3: { lat: 3.152659, lon: 101.702221 }, // Kuala Lumpur/Malasia
+    cam3: { lat: 3.118823, lon: 101.757088 }, // Kuala Lumpur/Malasia
     cam4: { lat: -53.785441, lon: -67.916667 }, // Rio Grande/Argentina
     cam5: { lat: -43.5276, lon: 172.637 }, // Christchurch/Nueva Zelanda
   };
@@ -25,7 +25,10 @@ export default async function getClima(opts = {}) {
   65: "Lluvia fuerte",
   80: "Chubascos ligeros",
   81: "Chubascos moderados",
-  82: "Chubascos violentos"
+  82: "Chubascos violentos",
+  95: "Tormenta eléctrica",
+  96: "Tormenta con granizo",
+  99: "Tormenta severa con granizo"
 };
 
 
